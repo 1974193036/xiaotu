ec4bfb503d69965db99122cbc7bcaf9f18177959
“376305” <shishuaiyun@deppon.com>
2024-04-24 17:27:02 +0800

    init project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-UA-Compatible',
+            value: 'IE=edge,chrome=1'
+          },
+        ]
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
